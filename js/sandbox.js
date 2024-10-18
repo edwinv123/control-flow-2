d219c50a1647bde4e627104a1bc746e83764e41d
edwinv123 <126evalentinnunez@frhsd.com>
2024-10-18 13:55:10 -0400

    final

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,9 +1,76 @@
 // for loop
 
-for(let i = 0; i < 5; i++){
-    console.log('in loop:',i);
-}
+//for(let i = 0; i < 5; i++){
+//    console.log('in loop:',i);
+//}
 
-console.log('loop finshed');
+//console.log('loop finshed');
 
-const names = ['shaun', 'mario', 'luigi'];
+// const names = ['shaun', 'mario', 'luigi'];
+
+// for(let i = 0; i < names.length; i++){
+//     //console.log(names[i]);
+//     let html = '<div>${names[i]}</div>';
+//     console.log(html);
+// }
+
+// const names = ['shaun', 'mario', 'luigi']
+// // let i = 0;
+
+// // while(i < 5>{
+// //     console.log('in loop: ', i);
+// //     i++;
+// // })
+
+// let i = 0;
+// while(i < names.length){
+//     console.log(names[i]);
+//     i++;
+// }
+
+// let i = 3;
+
+// do{
+//     console.log('val of i is: ',i);
+//     i++;
+// } while(i < 5);
+
+// if statements
+
+// const age = 25;
+
+// if(age > 20){
+//     console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 4){
+//     console.log("that's a lot of ninjas");
+// }
+
+// const password = 'p@ss';
+
+// if(password.length >= 12 && password.includes('@')){
+//     console.log('that password is might strong')
+// } else if(password.length >= 8 || password.includes('@') && password.length >= 5){
+// }
+
+// if(password.length >= 8){
+//     console.log('that password is long enough!')
+// } else {
+//     console.log('passwor is not long enough');
+// }
+
+// let user = false;
+
+// if(!user){
+//     console.log('you must be logged in to continue');
+// }
+
+// console.log(!true);
+// console.log(!false);
+
+const scores = [50, 25, 0, 30, 100, 20, 10];
+
+for(let i=0; i < scores.length; i++);
